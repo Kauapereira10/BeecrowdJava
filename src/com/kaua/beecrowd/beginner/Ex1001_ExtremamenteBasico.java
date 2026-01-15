2b583fb0768398c88ccc667f54e2c6859329d9ed
Kaua Pereira <borgeskaua1210@gmail.com>
2026-01-15 09:18:33 -0300

    feat: renomeia classe para Ex1001_ExtremamenteBasico

---
+++ src/com/kaua/beecrowd/beginner/Ex1001_ExtremamenteBasico.java
@@ -0,0 +1,16 @@
+package com.kaua.beecrowd.beginner;
+
+import java.util.Scanner;
+
+public class Ex1001_ExtremamenteBasico {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B =sc.nextInt();
+		int X = A + B;
+		
+		System.out.printf("X = %d\n", X);
+		
+		sc.close();
+	}
+}
