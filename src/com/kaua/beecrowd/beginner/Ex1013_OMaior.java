4e61c9b758f99d32d1b6709eec5d21f62ea23d4b
Kauã Pereira <borgeskaua1210@gmail.com>
2026-02-19 18:04:54 -0300

    feat: adiciona resolução do exercício 1013 - O Maior (Beecrowd)
    Implementa a solução do exercício 1013 - O Maior da plataforma Beecrowd.
    
    O programa lê três valores inteiros via entrada padrão e calcula o maior
    utilizando a fórmula matemática baseada em módulo (|a - b|).
    Ao final, exibe o maior valor no formato exigido pelo problema.

---
+++ src/com/kaua/beecrowd/beginner/Ex1013_OMaior.java
@@ -0,0 +1,20 @@
+package com.kaua.beecrowd.beginner;
+
+import java.util.Scanner;
+
+public class Ex1013_OMaior {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int AB = ((a + b) + Math.abs(a - b)) / 2;
+		int ABC = ((AB + c) + Math.abs(AB - c)) / 2;
+		
+		System.out.println(ABC + " eh o maior");
+		
+		sc.close();
+	}
+}
